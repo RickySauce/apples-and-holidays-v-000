8b3d1997d438e305bf58cc4e44b4a73671633679
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-25 15:30:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday|
     puts "#{season.capitalize}:"
     holiday.each do |day, details|
-
         puts "  #{day.to_s.gsub("_"," ").split(" "). each {|word| word.capitalize!}.join(" ")}: #{details.join(", ")}"
     end
   end
